9cc421032e11a26ef63a042eb0755b16904b0172
Sardar Ahmed <sardarbey0@gmail.com>
2026-08-04 12:57:31 +0500

    POSTGRES MIGRATION

--- alembic/env.py
+++ alembic/env.py
@@ -7,6 +7,11 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 from app.db.models import Base
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -22,7 +27,10 @@ target_metadata = Base.metadata
 
 # Set the DB URL directly here rather than relying on alembic.ini,
 # so there's one source of truth for the connection string.
-config.set_main_option("sqlalchemy.url", "sqlite+aiosqlite:///./app/db/finance.db")
+config.set_main_option(
+    "sqlalchemy.url",
+    os.getenv("DATABASE_URL")
+)
 
 
 def run_migrations_offline() -> None:
--- app/db/session.py
+++ app/db/session.py
@@ -1,10 +1,21 @@
 # app/db/session.py
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "sqlite+aiosqlite:///./app/db/finance.db"
+load_dotenv()
 
-engine = create_async_engine(DATABASE_URL, echo=False)
-AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=False
+)
+
+AsyncSessionLocal = async_sessionmaker(
+    engine,
+    expire_on_commit=False
+)
 
 async def get_session():
     async with AsyncSessionLocal() as session:
